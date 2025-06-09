c6e0b1cdf44cde6d5ff84751bd9096ab43ef4f42
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-06-09 02:15:50 +0200

    Developed Console type TO-DO TypeScript APP

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-type Task = {
-    id: number;
-    title: string;
-    done: boolean;
-}
-
+"use strict";
 ////////////////////////////////////////////////////////////
-
-let tasks: Task[] = [];
-
+let tasks = [];
 // fun to addtask 
-function addTask(title: string): void {
-    const newTask: Task = {
+function addTask(title) {
+    const newTask = {
         id: tasks.length + 1,
         title,
         done: false
@@ -18,39 +11,22 @@ function addTask(title: string): void {
     tasks.push(newTask);
     console.log(`Successfully added: "${title}"`);
 }
-
 // list of tasks
-function listTasks(): void {
+function listTasks() {
     console.log("\n All Tasks:");
     tasks.forEach((task) => {
         const status = task.done ? "done" : "not done";
         console.log(`${task.id}. ${task.title} - ${status}`);
     });
 }
-
 // fun to play with boolean now
-function markDone(id: number): void {
-    const task = tasks.find((t)=> t.id === id);
-
+function markDone(id) {
+    const task = tasks.find((t) => t.id === id);
     if (task) {
         task.done = true;
         console.log(` / Marked "${task.title}" as done`);
-    } else {
+    }
+    else {
         console.log(" X Task not Found, Please Check ");
     }
 }
-
-
-addTask("Learn Typescrit by developing TO-DO App")
-addTask("Build HTML and CSS boilerplate")
-addTask("CONNECT DOM with TS")
-addTask("Get working student in Germany")
-addTask("Learn Python advanced")
-addTask("Learn GenAI")
-listTasks()
-markDone(1)
-markDone(2)
-markDone(4)
-markDone(5)
-listTasks()
-
